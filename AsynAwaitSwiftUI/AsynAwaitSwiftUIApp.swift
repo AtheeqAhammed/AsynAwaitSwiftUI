8ce5ad2bf4225cc0f2d4571e1364633e71e3092d
AtheeqAhammed <atheeqahammed@gmail.com>
2024-11-17 14:11:02 +0530

    Initial Commit

---
+++ AsynAwaitSwiftUI/AsynAwaitSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsynAwaitSwiftUIApp.swift
+//  AsynAwaitSwiftUI
+//
+//  Created by Ateeq Ahmed on 17/11/24.
+//
+
+import SwiftUI
+
+@main
+struct AsynAwaitSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
